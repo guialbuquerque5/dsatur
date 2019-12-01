a04e864ea265b130d96a86b888f39114d787a875
Paulo <pieraquara@gmail.com>
2019-12-01 20:53:25 -0300

    Comentários para melhor entendimento do código

--- dsatur.py
+++ dsatur.py
@@ -1,6 +1,7 @@
 import time
 import sys
- 
+
+#Constroi o grafo a partir da csv 
 def build_graph(filename):
     g = []
     with open(filename) as f:
@@ -9,6 +10,7 @@ def build_graph(filename):
             g.append([int(x)-1 for x in row.split(',')[1:]])
     return g
 
+#Verifica se o argumento do programa tah certo
 if len(sys.argv) == 1:
     print('Error! To use this script you should pass the csv filename as argument!')
     print('ex: python dsatur.py grafo.csv')
@@ -16,18 +18,23 @@ if len(sys.argv) == 1:
 
 G = build_graph(sys.argv[1]) #build graph here
 
+#Numero de vertices
 n_v = len(G)
+#Lista de vertices
 V = [i for i in range(n_v)]
+#Lista de cores
 colors = [None] * n_v
-
+#funcao que retorna o grau de saturacao (?)
 order = lambda v: len(v[1][v[0]])
 
+#Funcao que retorna o grau de saturacao do vertice
 def d_sat(v):
     c = set()
     for _v in G[v]:
         c.add(colors[_v])
     return len(c) - 1
 
+#Retorna o grau do grafo
 def max_d_sat(v_list):
     r = {v: d_sat(v) for v in v_list}
     max_sat = 0
@@ -35,27 +42,32 @@ def max_d_sat(v_list):
        max_sat =r[v] if r[v] > max_sat else max_sat
     r = [v for v in r if r[v] == max_sat]
     return r
-    
+
+#Ordena o grafo com base no grau de saturacao
 def max_order(v_list,g):
     bigger = [v_list[0],0]
     for _v in v_list:
         bigger = [_v, order([_v, g])] if order([_v, g]) > bigger[1] else bigger
     return bigger[0]
 
+#Retorna um array com os vertices que sobraram, caso existam
 def next(v_list, g):
     if len(v_list) == 0:
         return None
     return max_order(max_d_sat(v_list),g)
 
+#Remove um vertice
 def remove_rel(v, g):
     for e in g:
         if v in g[e]:
             g[e].remove(v)
     return g
 
+#Printa o grafo
 def hash_graph(g):
     return { i: g[i] for i in range(len(g)) }
-    
+
+#retorna a cor minima do vertice
 def min_color(v):
     possible_colors = { x: None for x in range(5)}#no more than 5 different colors
     c = [colors[_v] for _v in G[v]]
@@ -66,28 +78,42 @@ def min_color(v):
             return _c
     return 0
 
+#O algoritmo em si, que retorna um array de cores onde cada index representa
+#um vertice, comecando pelo index 1 ao inves do 0
 def dsatur(V, g):
+    #_g eh o grafo
     _g = hash_graph(list(list(i) for i in g))
+    #_next eh o vertice com maior grau de saturacao
     _next = max_order(V,_g)
+    #a cor do primeiro eh 0
     colors[_next] = 0
     print(_next, 'cor: 0')
+    #remove o primeiro da lista de vertices
     V.remove(_next) 
+    #escolhe o proximo vertice
     _next = next([_v for _v in _g[_next] if colors[_v] is None], _g)
+    #remove ele do grafo
     _g = remove_rel(_next, _g)
     while len(V) > 0:
         print(_next, 'cor: ' + str(min_color(_next)) ) 
+        #define a cor do vertice como a menor cor possivel
         colors[_next] = min_color(_next)
+        #Remove o vertice da lista de vertices
         V.remove(_next)
+        #escolhe o proximo vertice
         _next = next( [_v for _v in V if colors[_v] is None], _g)
+        #remove ele do grafo
         _g = remove_rel(_next, _g)
+    #retorna o array de cores
     return colors
 
+#conta o tempo de execucao
 start_time = time.time()
-
-result_colors = dsatur(V,G)#execute algorith
-
+#roda o algoritmo
+result_colors = dsatur(V,G)
+#conta o tempo de execucao
 elapsed_time = time.time() - start_time
-
+#printa o array resultante
 print('Result colors:')
 print(result_colors)
 
